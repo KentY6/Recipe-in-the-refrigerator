1565a77301012373aa886cbeca049c578dd7331c
KentY <e6ony.ivory@gmail.com>
2023-03-07 20:29:28 +0900

    CssSettingsForMenuPage

--- src/screens/MenuPage.js
+++ src/screens/MenuPage.js
@@ -1,13 +1,83 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Title } from "../components/Title";
+import KitchenRoundedIcon from "@mui/icons-material/KitchenRounded";
+import ArrowDownwardRoundedIcon from "@mui/icons-material/ArrowDownwardRounded";
+import RestaurantRoundedIcon from "@mui/icons-material/RestaurantRounded";
+import ArrowUpwardRoundedIcon from "@mui/icons-material/ArrowUpwardRounded";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCarrot, faRightToBracket } from "@fortawesome/free-solid-svg-icons";
+import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 
 export const MenuPage = () => {
   return (
-    <div>
-      <Link to={`/recipesPage`}>レシピページ</Link>
-      <Link to={`/addFoodsPage`}>食材追加ページ</Link>
-      <Link to={`/loginForm`}>ログインフォーム</Link>
-      <Link to={`/freeRecipesPage`}>フリーレシピページ</Link>
+    <div className="refrigerator">
+      <div className="mainTitle">
+        <Title title={`Recipe in the refrigerator`} />
+      </div>
+      <div className="upperContainer">
+        <div className="recipesContainer">
+          <Link to={`/recipesPage`} className="linkRange">
+            <div className="iconsContainer">
+              <div className="upperIcons">
+                <div className="boxIcon">
+                  <KitchenRoundedIcon fontSize="80px" />
+                </div>
+                <div className="arrowIcon">
+                  <ArrowDownwardRoundedIcon fontSize="100px" />
+                </div>
+                <div className="recipeIcon">
+                  <RestaurantRoundedIcon fontSize="100px" />
+                </div>
+              </div>
+            </div>
+          </Link>
+        </div>
+        <div className="foodsContainer">
+          <Link to={`/addFoodsPage`} className="linkRange">
+            <div className="iconsContainer">
+              <div className="upperIcons">
+                <div className="boxIcon">
+                  <KitchenRoundedIcon fontSize="80px" />
+                </div>
+                <div className="arrowIcon">
+                  <ArrowUpwardRoundedIcon fontSize="80px" />
+                </div>
+                <div className="carrot">
+                  <FontAwesomeIcon icon={faCarrot} fontSize="60px" />
+                </div>
+              </div>
+            </div>
+          </Link>
+        </div>
+      </div>
+      <div className="lowerContainer">
+        <div className="loginContainer">
+          <Link to={`/loginForm`} className="linkRange">
+            <div className="iconsContainer">
+              <div className="loginIconText">
+                <div className="loginIcon">
+                  <FontAwesomeIcon icon={faRightToBracket} />
+                </div>
+                <div className="loginText">ログイン</div>
+              </div>
+            </div>
+          </Link>
+        </div>
+
+        <div className="freeRecipesContainer">
+          <Link to={`/freeRecipesPage`} className="linkRange">
+            <div className="iconsContainer">
+              <div className="freeRecipeIconText">
+                <div className="freeRecipeIcon">
+                  <SearchRoundedIcon fontSize="40px" />
+                </div>
+                <div className="freeRecipeText">フリーレシピ検索</div>
+              </div>
+            </div>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
