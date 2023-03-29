cb0ab7f602dee25102e16795a19d169825aae13d
KentY <e6ony.ivory@gmail.com>
2023-03-29 18:34:33 +0900

    freeRecipesPage/cssSettings

--- src/components/FreeRecipesButton.js
+++ src/components/FreeRecipesButton.js
@@ -3,7 +3,7 @@ import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 
 export const FreeRecipesButton = () => {
   return (
-    <div>
+    <div className="freeRecipesButton">
       <SearchRoundedIcon />
       <div>選択した食材でレシピ検索</div>
     </div>
