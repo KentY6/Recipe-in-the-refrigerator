eec7389c6e98b2528d57308cbc240d60fc6c607c
KentY <e6ony.ivory@gmail.com>
2023-04-09 19:58:17 +0900

    SignUpFunctionImplemented

--- src/components/AuthenticationInputForm.js
+++ src/components/AuthenticationInputForm.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const AuthenticationInputForm = ({ text }) => {
+export const AuthenticationInputForm = ({ text, getText }) => {
+  const [authenticationText, setAuthenticationText] = useState("");
+
+  const getInputText = (e) => {
+    e.preventDefault();
+    getText(authenticationText);
+  };
+
   return (
     <div>
       <div>{text}</div>
-      <input type="text" />
+      <form onChange={getInputText}>
+        <input
+          type="text"
+          value={authenticationText}
+          onChange={(e) => setAuthenticationText(e.target.value)}
+        />
+      </form>
     </div>
   );
 };
--- src/screens/LoginForm.js
+++ src/screens/LoginForm.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import UndoRoundedIcon from "@mui/icons-material/UndoRounded";
 import { Link } from "react-router-dom";
 import { PageTitle } from "../components/PageTitle";
 import { AuthenticationInputForm } from "../components/AuthenticationInputForm";
+import { auth } from "../firebase";
 
 export const LoginForm = ({ resetFreeRecipes }) => {
+  const [mailAddress, setMailAddress] = useState("");
+  const [passWord, setPassWord] = useState("");
+
+  const getMailAddressText = (text) => {
+    setMailAddress(text);
+  };
+
+  const getPassWordText = (text) => {
+    setPassWord(text);
+  };
+
+  const signIn = () => {
+    auth.createUserWithEmailAndPassword(mailAddress, passWord);
+  };
+
   return (
     <div>
       <div className="returnAndTitle">
@@ -16,12 +32,18 @@ export const LoginForm = ({ resetFreeRecipes }) => {
         <PageTitle PageTitle={"ログイン"} />
       </div>
       <div className="loginForm">
-        <AuthenticationInputForm text={"・メールアドレス"} />
-        <AuthenticationInputForm text={"・パスワード"} />
+        <AuthenticationInputForm
+          text={"・メールアドレス"}
+          getText={getMailAddressText}
+        />
+        <AuthenticationInputForm
+          text={"・パスワード"}
+          getText={getPassWordText}
+        />
       </div>
       <div className="authenticationButton">
         <button>ログイン</button>
-        <button>ログアウト</button>
+        <button onClick={signIn}>サインアップ</button>
       </div>
     </div>
   );
