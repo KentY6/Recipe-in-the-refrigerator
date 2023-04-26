dbbe9c49b7c99fb7ceecf3eff6b9132e8a1731a4
KentY <e6ony.ivory@gmail.com>
2023-04-26 15:32:20 +0900

    addResponsiveDesign

--- src/screens/MenuPage.js
+++ src/screens/MenuPage.js
@@ -11,73 +11,75 @@ import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 
 export const MenuPage = ({ logInState }) => {
   return (
-    <div className="refrigerator">
-      <div className="mainTitle">
-        <MainTitle MainTitle={`Recipe in the refrigerator`} />
-      </div>
-      <div className="upperContainer">
-        <div className="recipesContainer">
-          <Link to={`/recipesPage`} className="linkRange">
-            <div className="iconsContainer">
-              <div className="upperIcons">
-                <div className="boxIcon">
-                  <KitchenRoundedIcon fontSize="80px" />
-                </div>
-                <div className="arrowIcon">
-                  <ArrowDownwardRoundedIcon fontSize="100px" />
-                </div>
-                <div className="recipeIcon">
-                  <RestaurantRoundedIcon fontSize="100px" />
+    <div className="menuPage">
+      <div className="refrigerator">
+        <div className="mainTitle">
+          <MainTitle MainTitle={`Recipe in the refrigerator`} />
+        </div>
+        <div className="upperContainer">
+          <div className="recipesContainer">
+            <Link to={`/recipesPage`} className="linkRange">
+              <div className="iconsContainer">
+                <div className="upperIcons">
+                  <div className="boxIcon">
+                    <KitchenRoundedIcon fontSize="80px" />
+                  </div>
+                  <div className="arrowIcon">
+                    <ArrowDownwardRoundedIcon fontSize="100px" />
+                  </div>
+                  <div className="recipeIcon">
+                    <RestaurantRoundedIcon fontSize="100px" />
+                  </div>
                 </div>
               </div>
-            </div>
-          </Link>
-        </div>
-        <div className="foodsContainer">
-          <Link to={`/addFoodsPage`} className="linkRange">
-            <div className="iconsContainer">
-              <div className="upperIcons">
-                <div className="boxIcon">
-                  <KitchenRoundedIcon fontSize="80px" />
-                </div>
-                <div className="arrowIcon">
-                  <ArrowUpwardRoundedIcon fontSize="80px" />
-                </div>
-                <div className="carrot">
-                  <FontAwesomeIcon icon={faCarrot} fontSize="60px" />
-                </div>
-              </div>
-            </div>
-          </Link>
-        </div>
-      </div>
-      <div className="lowerContainer">
-        <div className="loginContainer">
-          <Link to={`/loginForm`} className="linkRange">
-            <div className="iconsContainer">
-              <div className="loginIconText">
-                <div className="loginIcon">
-                  <FontAwesomeIcon icon={faRightToBracket} />
-                </div>
-                <div className="loginText">
-                  {logInState === false ? "ログイン" : "ログアウト"}
+            </Link>
+          </div>
+          <div className="foodsContainer">
+            <Link to={`/addFoodsPage`} className="linkRange">
+              <div className="iconsContainer">
+                <div className="upperIcons">
+                  <div className="boxIcon">
+                    <KitchenRoundedIcon fontSize="80px" />
+                  </div>
+                  <div className="arrowIcon">
+                    <ArrowUpwardRoundedIcon fontSize="80px" />
+                  </div>
+                  <div className="carrot">
+                    <FontAwesomeIcon icon={faCarrot} fontSize="60px" />
+                  </div>
                 </div>
               </div>
-            </div>
-          </Link>
+            </Link>
+          </div>
         </div>
+        <div className="lowerContainer">
+          <div className="loginContainer">
+            <Link to={`/loginForm`} className="linkRange">
+              <div className="iconsContainer">
+                <div className="loginIconText">
+                  <div className="loginIcon">
+                    <FontAwesomeIcon icon={faRightToBracket} />
+                  </div>
+                  <div className="loginText">
+                    {logInState === false ? "ログイン" : "ログアウト"}
+                  </div>
+                </div>
+              </div>
+            </Link>
+          </div>
 
-        <div className="freeRecipesContainer">
-          <Link to={`/freeRecipesPage`} className="linkRange">
-            <div className="iconsContainer">
-              <div className="freeRecipeIconText">
-                <div className="freeRecipeIcon">
-                  <SearchRoundedIcon fontSize="40px" />
+          <div className="freeRecipesContainer">
+            <Link to={`/freeRecipesPage`} className="linkRange">
+              <div className="iconsContainer">
+                <div className="freeRecipeIconText">
+                  <div className="freeRecipeIcon">
+                    <SearchRoundedIcon fontSize="40px" />
+                  </div>
+                  <div className="freeRecipeText">フリーレシピ検索</div>
                 </div>
-                <div className="freeRecipeText">フリーレシピ検索</div>
               </div>
-            </div>
-          </Link>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
